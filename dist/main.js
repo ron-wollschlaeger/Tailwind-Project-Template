9773774ae31d063ec8bfa10c1bcb946b3011a059
pxMahio <ron.wollschlaeger@frontshape.de>
2023-09-21 21:14:07 +0200

    Update template with JS

---
+++ dist/main.js
@@ -0,0 +1,48 @@
+// Icons
+const sunIcon = document.querySelector(".sun");
+const moonIcon = document.querySelector(".moon");
+
+// Theme Vars
+const userTheme = localStorage.getItem("theme");
+const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+// Icon Toggling
+const iconToggle = () => {
+    moonIcon.classList.toggle("display-none");
+    sunIcon.classList.toggle("display-none");
+};
+
+// Initial Theme Check
+const themeCheck = () => {
+    if (userTheme === "dark" || (!userTheme && systemTheme)) {
+        document.documentElement.classList.add("dark");
+        moonIcon.classList.add("display-none");
+        return;
+    }
+    sunIcon.classList.add("display-none"); 
+};
+
+// Manual Theme Switch
+const themeSwitch = () => {
+    if (document.documentElement.classList.contains("dark")) {
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("theme", "light");
+        iconToggle();
+        return;
+    }
+    document.documentElement.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+    iconToggle();
+};
+
+// call theme switch on clicking buttons
+sunIcon.addEventListener("click", () => {
+    themeSwitch();
+})
+
+moonIcon.addEventListener("click", () => {
+    themeSwitch();
+})
+
+// invoke theme check on initial load
+themeCheck();
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./dist/*.{html,js}"],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+      }
+    },
   },
   plugins: [],
-}
-
+};
